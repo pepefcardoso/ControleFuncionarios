a7645d05d5a7ec8613acfb593933041479f51370
Pepefcardoso <pppfcardoso@gmail.com>
2022-12-09 13:31:12 -0300

    Criação da classe abstrata DAO e da classe DaoFuncionarios

---
+++ dao/dao_funcionarios.py
@@ -0,0 +1,21 @@
+from dao.abstract_dao import Dao
+from entidade.funcionario import Funcionario
+
+
+class DaoFuncionarios(Dao):
+    def __init__(self):
+        super().__init__('funcionarios.pkl')
+
+    def add(self, funcionario: Funcionario):
+        if (funcionario is not None and
+            isinstance(funcionario, Funcionario) and
+            isinstance(funcionario.cpf, str)):
+            super().add(funcionario.cpf, funcionario)
+
+    def get(self, key: str):
+        if isinstance(key, str):
+            return super().get(key)
+
+    def remove(self, key: str):
+        if isinstance(key, str):
+            return super().remove(key)
