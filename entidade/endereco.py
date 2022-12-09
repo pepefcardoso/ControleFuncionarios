8ef7923403c6b5a06f1d27a774a78ad1f614f58d
Pepefcardoso <pppfcardoso@gmail.com>
2022-12-09 13:03:31 -0300

    Criação da classe Endereço e implementação dos getter/setter das classes Funcionario e Endereco

---
+++ entidade/endereco.py
@@ -0,0 +1,85 @@
+class Endereco():
+    def __init__(self, cep: str, estado: str,
+                 municipio: str, bairro: str,
+                 tipo_logradouro: str, logradouro: str,
+                 numero: str, complemento: str):
+        self.__cep = cep
+        self.__estado = estado
+        self.__municipio = municipio
+        self.__bairro = bairro
+        self.__tipo_logradouro = tipo_logradouro
+        self.__logradouro = logradouro
+        self.__numero = numero
+        self.__complemento = complemento
+
+    @property
+    def cep(self):
+        return self.__cep
+
+    @cep.setter
+    def cep(self,cep):
+        if isinstance(cep, str):
+            self.__cep = cep
+
+    @property
+    def estado(self):
+        return self.__estado
+
+    @estado.setter
+    def estado(self,estado):
+        if isinstance(estado, str):
+            self.__estado = estado
+
+    @property
+    def municipio(self):
+        return self.__municipio
+
+    @municipio.setter
+    def municipio(self,municipio):
+        if isinstance(municipio, str):
+            self.__municipio = municipio
+
+    @property
+    def bairro(self):
+        return self.__bairro
+
+    @bairro.setter
+    def bairro(self,bairro):
+        if isinstance(bairro, str):
+            self.__bairro = bairro
+
+    @property
+    def tipo_logradouro(self):
+        return self.__tipo_logradouro
+
+    @tipo_logradouro.setter
+    def tipo_logradouro(self,tipo_logradouro):
+        if isinstance(tipo_logradouro, str):
+            self.__tipo_logradouro = tipo_logradouro
+
+    @property
+    def logradouro(self):
+        return self.__logradouro
+
+    @logradouro.setter
+    def logradouro(self,logradouro):
+        if isinstance(logradouro, str):
+            self.__logradouro = logradouro
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @numero.setter
+    def numero(self,numero):
+        if isinstance(numero, str):
+            self.__numero = numero
+
+    @property
+    def complemento(self):
+        return self.__complemento
+
+    @complemento.setter
+    def complemento(self,complemento):
+        if isinstance(complemento, str):
+            self.__complemento = complemento
--- entidade/funcionario.py
+++ entidade/funcionario.py
@@ -5,10 +5,127 @@ from entidade.endereco import Endereco
 class Funcionario():
     def __init__(self, matricula: str,nome: str,
                  sexo: str, rg: str, cpf: str,
-                 pis: str, nascimento: date,
+                 pis: str, data_nascimento: date,
                  endereco: Endereco, email: str,
-                 telefone: str, contato_emergencial: str,
-                 telefone_emergencial: str):
+                 telefone: str, nome_contato_emergencial: str,
+                 telefone_contato_emergencial: str):
         self.__matricula = matricula
-        self.__local = local
-        self.__
+        self.__nome = nome
+        self.__sexo = sexo
+        self.__rg = rg
+        self.__cpf = cpf
+        self.__pis = pis
+        self.__data_nascimento = data_nascimento
+        self.__endereco = endereco
+        self.__email = email
+        self.__telefone = telefone
+        self.__nome_contato_emergencial = nome_contato_emergencial
+        self.__telefone_contato_emergencial = telefone_contato_emergencial
+
+    @property
+    def matricula(self):
+        return self.__matricula
+
+    @matricula.setter
+    def matricula(self,matricula):
+        if isinstance(matricula,str):
+            self.__matricula = matricula
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self,nome):
+        if isinstance(nome,str):
+            self.__nome = nome
+
+    @property
+    def sexo(self):
+        return self.__sexo
+
+    @sexo.setter
+    def sexo(self,sexo):
+        if isinstance(sexo,str):
+            self.__sexo = sexo
+
+    @property
+    def rg(self):
+        return self.__rg
+
+    @rg.setter
+    def rg(self,rg):
+        if isinstance(rg,str):
+            self.__rg = rg
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self,cpf):
+        if isinstance(cpf,str):
+            self.__cpf = cpf
+
+    @property
+    def pis(self):
+        return self.__pis
+
+    @pis.setter
+    def pis(self,pis):
+        if isinstance(pis,str):
+            self.__pis = pis
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+
+    @data_nascimento.setter
+    def data_nascimento(self,data_nascimento):
+        if isinstance(data_nascimento,date):
+            self.__data_nascimento = data_nascimento
+
+    @property
+    def endereco(self):
+        return self.__endereco
+
+    @endereco.setter
+    def endereco(self,endereco):
+        if isinstance(endereco,Endereco):
+            self.__endereco = endereco
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self,email):
+        if isinstance(email,str):
+            self.__email = email
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self,telefone):
+        if isinstance(telefone,str):
+            self.__telefone = telefone
+
+    @property
+    def nome_contato_emergencial(self):
+        return self.__nome_contato_emergencial
+
+    @nome_contato_emergencial.setter
+    def nome_contato_emergencial(self,nome_contato_emergencial):
+        if isinstance(nome_contato_emergencial,str):
+            self.__nome_contato_emergencial = nome_contato_emergencial
+
+    @property
+    def telefone_contato_emergencial(self):
+        return self.__telefone_contato_emergencial
+
+    @telefone_contato_emergencial.setter
+    def telefone_contato_emergencial(self,telefone_contato_emergencial):
+        if isinstance(telefone_contato_emergencial,str):
+            self.__telefone_contato_emergencial = telefone_contato_emergencial
